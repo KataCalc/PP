0c37a074f6ea757c244fc14dee117028a67ac75e
Senin22 <alexandroff03@ya.ru>
2022-09-02 21:53:34 +0300

    Task-1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -16,43 +16,44 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
 
 
     public void createUsersTable() {   //сoздать таблицу узеров +++++
-        String sql = "CREATE TABLE LUSER(ID INT NOT NULL AUTO_INCREMENT, 'NAME' VARCHAR(20), LASTNAME VARCHAR(20), AGE INT)";
+        String sql = "CREATE TABLE IF NOT EXISTS LUSER(ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, FIRSTNAME VARCHAR(20), LASTNAME VARCHAR(20), AGE TINYINT)";
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+//            try {
+//                connection.rollback();
+//            } catch (SQLException ex) {
+//                throw new RuntimeException(ex);
+//            }
         }
     }
 
     public void dropUsersTable() {  //удаления таблицы ++++
         try (Statement statement = connection.createStatement()) {
-            String sql = "DROP TABLE LUSER";
+            String sql = "DROP TABLE IF EXISTS LUSER";
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException { //добавление юзера в базу данных и сохранение
-        String sql = "INSERT INTO LUSER('NAME',LASTNAME,AGE)VALUES(?,?,?)";
+
         PreparedStatement preparedStatement = null;
         try {
+            String sql = "INSERT INTO LUSER(FIRSTNAME,LASTNAME,AGE)VALUES(?,?,?)";
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-
+            preparedStatement.execute();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-
+            connection.rollback();
         }
 
     }
@@ -62,7 +63,8 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         Statement statement = null;
         try {
             statement = connection.createStatement();
-            statement.executeUpdate("DELETE FROM LUSER WHERE ID 'id'");
+            statement.executeUpdate("DELETE FROM LUSER WHERE ID = id");
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -71,36 +73,31 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
 
     public List<User> getAllUsers() throws SQLException {//достать всех пользователей из базы данных
         List<User> userList = new ArrayList<>();
-        String sql = "SELECT 'NAME',LASTNAME, AGE FROM LUSER";
+        String sql = "SELECT FIRSTNAME,LASTNAME, AGE FROM LUSER";
         Statement statement = null;
         try {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 User user1 = new User();
-                user1.setName(resultSet.getString("NAME"));
+                user1.setName(resultSet.getString("FIRSTNAME"));
                 user1.setLastName(resultSet.getString("LASTNAME"));
                 user1.setAge(resultSet.getByte("AGE"));
                 userList.add(user1);
+
             }
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (statement != null) {
-                statement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-
         }
         return userList;
     }
 
     public void cleanUsersTable() { //очищения таблицы пользователей
-        try (Statement statement = connection.createStatement()){
+        try (Statement statement = connection.createStatement()) {
             String sql = "TRUNCATE TABLE  LUSER";
             statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,20 +7,29 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String DRIVER_1 = "com.mysql.jdbc.Driver";
-    private static final String USERNAME_1 = "noon-root";
-    private static final String PASSWORD_1 = "root";
+    private static final String DRIVER_1 = "com.mysql.cj.jdbc.Driver";
+    private static final String USERNAME_1 = "root";
+    private static final String PASSWORD_1 = "root1";
     private static final String URL_1 = "jdbc:mysql://localhost:3306/dbluser";
 
 public Connection getConnection(){
     Connection connection = null;
     try {
+
         Class.forName(DRIVER_1);
         connection = DriverManager.getConnection(URL_1,USERNAME_1,PASSWORD_1);
         System.out.println("Connection OK");
+        connection.setAutoCommit(false);
+        connection.commit();
+
     } catch (ClassNotFoundException | SQLException e) {
         System.out.println("Connection ERROR");
-        throw new RuntimeException(e);
+        try {
+            connection.rollback();
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
 
     }
     return connection;
